23dfd4ce242e921e428a7a620cdd87dec791627c
amingoli78 <amin.goli78@gmail.com>
2019-09-08 16:50:00 +0430

    fix again

--- app/src/main/java/com/ermile/salamquran/Static/lookServer.java
+++ app/src/main/java/com/ermile/salamquran/Static/lookServer.java
@@ -3,6 +3,6 @@ package com.ermile.salamquran.Static;
 public class lookServer {
 
 
-    public static String appkey0 = "74165249ad965f5";
+    public static String appkey0 = "74165249ad965f5.   ";
     public static String appkey = appkey0+"06c5234324e870fd3";
 }
