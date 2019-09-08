9cf14702cee758dcf5fca3efccf5c1d07dd4d9f0
amingoli78 <amin.goli78@gmail.com>
2019-09-08 17:19:47 +0430

    fix other

--- app/src/main/java/com/ermile/salamquran/Static/lookServer.java
+++
@@ -1,8 +0,0 @@
-package com.ermile.salamquran.Static;
-
-public class lookServer {
-
-
-    public static String appkey0 = "74165249ad965f5.   ";
-    public static String appkey = appkey0+"06c5234324e870fd3";
-}
