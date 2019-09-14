1ab894fbd751f3291fd66fd222a5791096282471
amingoli78 <amin.goli78@gmail.com>
2019-09-14 19:22:59 +0430

    add viedoPlayer Fetcher

--- app/src/main/java/com/ermile/salamquran/Actitvty/View/Video.java
+++ app/src/main/java/com/ermile/salamquran/Actitvty/View/Video.java
@@ -1,8 +1,11 @@
 package com.ermile.salamquran.Actitvty.View;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.ermile.salamquran.R;
 
@@ -12,5 +15,17 @@ public class Video extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video);
+
+        VideoView videoView = (VideoView)findViewById(R.id.your_video_view);
+        MediaController mc = new MediaController(this);
+        videoView.setMediaController(mc);
+
+        String str = getIntent().getStringExtra("url");
+        Uri uri = Uri.parse(str);
+
+        videoView.setVideoURI(uri);
+
+        videoView.requestFocus();
+        videoView.start();
     }
 }
