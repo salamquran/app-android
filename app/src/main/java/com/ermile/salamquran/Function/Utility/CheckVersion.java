2a9d7c046e5ef2f7fb45494cad2de121c4a8eac8
amingoli78 <amin.goli78@gmail.com>
2019-10-20 19:25:11 +0330

    fix if Is null 'api/deprecate/version' no crash

--- app/src/main/java/com/ermile/salamquran/Function/Utility/CheckVersion.java
+++ app/src/main/java/com/ermile/salamquran/Function/Utility/CheckVersion.java
@@ -26,29 +26,36 @@ public class CheckVersion {
             /*Url For Update*/
             final String urlUpdate = url.getString("update");
             /*Deprecate Value*/
-            String deprecatedVersion = version.getString("deprecated").replace(".","");
-            int Depver = Integer.valueOf(deprecatedVersion);
-            String deprecated_title = version.getString("deprecated_title");
-            String deprecated_desc = version.getString("deprecated_desc");
-            String deprecated_btnTitle = "Update Now!";
-            /*Update Value*/
-            String lastVersion = version.getString("last").replace(".","");
-            int Updver = Integer.valueOf(lastVersion);
-            String update_title = version.getString("update_title");
-            String update_desc = version.getString("update_desc");
-            if (value.versionCode <= Depver){
-                Log.d(tag.function, "deprecated Version "+value.versionCode+" = "+Depver);
-                SaveManager.get(context).change_deprecatedVersion(true);
-                Intent openURL = new Intent ( Intent.ACTION_VIEW );
-                openURL.setData (Uri.parse( urlUpdate ));
-                new Dialog(context,deprecated_title,deprecated_desc,deprecated_btnTitle,false,openURL);
-                return true;
+            if (!version.isNull("deprecated")){
+                String deprecatedVersion = version.getString("deprecated").replace(".","");
+                int Depver = Integer.valueOf(deprecatedVersion);
+                String deprecated_title = version.getString("deprecated_title");
+                String deprecated_desc = version.getString("deprecated_desc");
+                String deprecated_btnTitle = "Update Now!";
+                /*Update Value*/
+                String lastVersion = version.getString("last").replace(".","");
+                int Updver = Integer.valueOf(lastVersion);
+                String update_title = version.getString("update_title");
+                String update_desc = version.getString("update_desc");
+                if (value.versionCode <= Depver){
+                    Log.d(tag.function, "deprecated Version "+value.versionCode+" = "+Depver);
+                    SaveManager.get(context).change_deprecatedVersion(true);
+                    Intent openURL = new Intent ( Intent.ACTION_VIEW );
+                    openURL.setData (Uri.parse( urlUpdate ));
+                    new Dialog(context,deprecated_title,deprecated_desc,deprecated_btnTitle,false,openURL);
+                    return true;
 
-            }else {
-                SaveManager.get(context).change_deprecatedVersion(false);
-                updateVersion(context,Updver);
+                }else {
+                    SaveManager.get(context).change_deprecatedVersion(false);
+                    updateVersion(context,Updver);
+                    return false;
+                }
+            }
+            else {
                 return false;
             }
+
+
         } catch (JSONException | IOException e) {
             e.printStackTrace();
             return false;
