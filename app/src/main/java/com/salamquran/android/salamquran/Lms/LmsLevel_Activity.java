36651b6382a621ecdb77b8f9023aabdf9a0a7a32
amingoli78 <amin.goli78@gmail.com>
2019-12-14 20:39:01 +0330

    add LmsLevel Activity

---
+++ app/src/main/java/com/salamquran/android/salamquran/Lms/LmsLevel_Activity.java
@@ -0,0 +1,16 @@
+package com.salamquran.android.salamquran.Lms;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.salamquran.android.R;
+
+public class LmsLevel_Activity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_lms_level);
+  }
+}
