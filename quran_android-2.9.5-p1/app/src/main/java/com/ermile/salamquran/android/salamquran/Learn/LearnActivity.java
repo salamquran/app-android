88baed866dbf3a26b264ebe80ade2060293f8bf5
amingoli78 <amin.goli78@gmail.com>
2019-12-15 15:57:05 +0330

    add Activity Learn (Show Level List)

---
+++ quran_android-2.9.5-p1/app/src/main/java/com/ermile/salamquran/android/salamquran/Learn/LearnActivity.java
@@ -0,0 +1,16 @@
+package com.ermile.salamquran.android.salamquran.Learn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ermile.salamquran.android.R;
+
+public class LearnActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_learn);
+  }
+}
