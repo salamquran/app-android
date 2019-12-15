ee43fa3b154a8dd04ee80d0b6249be65be529b69
amingoli78 <amin.goli78@gmail.com>
2019-12-15 15:39:23 +0330

    add SaveManager

---
+++ quran_android-2.9.5-p1/app/src/main/java/com/ermile/salamquran/android/salamquran/SaveManager.java
@@ -0,0 +1,64 @@
+package com.ermile.salamquran.android.salamquran;
+
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.content.SharedPreferences;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SaveManager extends ContextWrapper {
+
+  SharedPreferences.Editor editor;
+  SharedPreferences sharedPreferences;
+  public static final String SH_PREF_NAME = "ShPerfManager_Payamres";
+
+
+  @SuppressLint("CommitPrefEdits")
+  private SaveManager(Context context) {
+    super(context);
+    sharedPreferences = getSharedPreferences(SH_PREF_NAME, MODE_PRIVATE);
+    editor = sharedPreferences.edit();
+  }
+  public static SaveManager get(Context context) {
+    return new SaveManager(context);
+  }
+
+
+  /** App Info */
+  public static final String local = "local";
+  public static final String apikey = "apikey";
+  public static final String userCode = "userCode";
+  public static final String zonId = "zonId";
+  public static final String mobile = "mobile";
+
+  public void save_local_url(String local_URL) {
+    editor.putString(local, local_URL);
+    editor.apply();
+  }
+  public void save_user_info(String Apikey, String UserCode, String ZonId, String Mobile) {
+    editor.putString( apikey, Apikey);
+    editor.putString( userCode, UserCode );
+    editor.putString( zonId, ZonId);
+    editor.putString( mobile, Mobile);
+    editor.apply();
+  }
+
+  public Map<String, String> getstring_appINFO() {
+    HashMap<String, String> hashMap = new HashMap<>();
+    hashMap.put(local, sharedPreferences.getString(local, url.api ));
+
+    hashMap.put(apikey, sharedPreferences.getString(apikey, null));
+    hashMap.put(userCode, sharedPreferences.getString(userCode, null));
+    hashMap.put(zonId, sharedPreferences.getString(zonId, null));
+    hashMap.put(mobile, sharedPreferences.getString(mobile, null));
+    return hashMap;
+  }
+
+
+
+}
+
+
