7423b7fd1bf8a23375a0749b21472e529c589ab3
amingoli78 <amin.goli78@gmail.com>
2019-12-16 14:55:15 +0330

    add Activity

---
+++ quran_android-2.9.5-p1/app/src/main/java/com/ermile/salamquran/android/salamquran/Mag/MagActivity.java
@@ -0,0 +1,16 @@
+package com.ermile.salamquran.android.salamquran.Mag;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ermile.salamquran.android.R;
+
+public class MagActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_mag);
+  }
+}
