b0bff7dca2b103b008538e7ffd1d80e774628016
amingoli78 <amin.goli78@gmail.com>
2019-12-17 20:06:11 +0330

    fix SearchFragment

--- quran_android-2.9.5-p1/app/src/main/java/com/ermile/salamquran/android/salamquran/SearchFragment.java
+++ quran_android-2.9.5-p1/app/src/main/java/com/ermile/salamquran/android/salamquran/SearchFragment.java
@@ -4,9 +4,16 @@ package com.ermile.salamquran.android.salamquran;
 import android.app.SearchManager;
 import android.content.ComponentName;
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
 
+import android.text.Html;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.SearchView;
+
+import androidx.core.content.ContextCompat;
 import androidx.core.view.MenuItemCompat;
 import androidx.fragment.app.Fragment;
 
@@ -14,6 +21,7 @@ import android.view.LayoutInflater;
 import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.ermile.salamquran.android.R;
 import com.ermile.salamquran.android.SearchActivity;
@@ -37,8 +45,21 @@ public class SearchFragment extends Fragment {
     // Inflate the layout for this fragment
     View view = inflater.inflate(R.layout.fragment_search, container, false);
 
+
+
     final SearchView searchView = view.findViewById(R.id.searchView);
-    final SearchManager searchManager = (SearchManager) getActivity().getSystemService(Context.SEARCH_SERVICE);
+
+    int id = searchView
+        .getContext()
+        .getResources()
+        .getIdentifier("android:id/search_src_text", null, null);
+    TextView textView = (TextView) searchView.findViewById(id);
+//    textView.setTextColor(Color.WHITE);
+    Typeface typeface = Typeface.createFromAsset(getContext().getAssets(),"dana_regular.ttf");
+    textView.setTypeface(typeface);
+
+    final SearchManager searchManager =
+        (SearchManager) getActivity().getSystemService(Context.SEARCH_SERVICE);
     searchView.setQueryHint(getString(R.string.search_hint));
     searchView.setSearchableInfo(searchManager.getSearchableInfo(
         new ComponentName(getActivity().getApplication(), SearchActivity.class)));
