57d09247ca84ccdc9c92dba635e5d35a522ba6f8
amingoli78 <amin.goli78@gmail.com>
2019-12-17 18:47:20 +0330

    fix Search Fragment

--- quran_android-2.9.5-p1/app/src/main/java/com/ermile/salamquran/android/salamquran/SearchFragment.java
+++ quran_android-2.9.5-p1/app/src/main/java/com/ermile/salamquran/android/salamquran/SearchFragment.java
@@ -1,15 +1,24 @@
 package com.ermile.salamquran.android.salamquran;
 
 
+import android.app.SearchManager;
+import android.content.ComponentName;
+import android.content.Context;
 import android.os.Bundle;
 
+import android.widget.SearchView;
+import androidx.core.view.MenuItemCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.ermile.salamquran.android.R;
+import com.ermile.salamquran.android.SearchActivity;
+
+import java.util.Objects;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -26,7 +35,16 @@ public class SearchFragment extends Fragment {
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
                            Bundle savedInstanceState) {
     // Inflate the layout for this fragment
-    return inflater.inflate(R.layout.fragment_search, container, false);
+    View view = inflater.inflate(R.layout.fragment_search, container, false);
+
+    final SearchView searchView = view.findViewById(R.id.searchView);
+    final SearchManager searchManager = (SearchManager) getActivity().getSystemService(Context.SEARCH_SERVICE);
+    searchView.setQueryHint(getString(R.string.search_hint));
+    searchView.setSearchableInfo(searchManager.getSearchableInfo(
+        new ComponentName(getActivity().getApplication(), SearchActivity.class)));
+
+
+    return view;
   }
 
 }
