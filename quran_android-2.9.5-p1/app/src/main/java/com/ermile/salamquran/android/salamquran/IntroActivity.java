10ab0869510ed7489b5c189348f215e6ce565e2c
amingoli78 <amin.goli78@gmail.com>
2019-12-18 19:26:09 +0330

    add introActivity

---
+++ quran_android-2.9.5-p1/app/src/main/java/com/ermile/salamquran/android/salamquran/IntroActivity.java
@@ -0,0 +1,16 @@
+package com.ermile.salamquran.android.salamquran;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ermile.salamquran.android.R;
+
+public class IntroActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_intro);
+  }
+}
--- quran_android-2.9.5-p1/app/src/main/java/com/ermile/salamquran/android/salamquran/Splash.java
+++ quran_android-2.9.5-p1/app/src/main/java/com/ermile/salamquran/android/salamquran/Splash.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.ermile.salamquran.android.QuranDataActivity;
 import com.ermile.salamquran.android.R;
@@ -16,6 +17,7 @@ public class Splash extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_splash);
     new TempLoginUtil(getApplication());
+    nextActivity();
   }
 
   private void nextActivity(){
