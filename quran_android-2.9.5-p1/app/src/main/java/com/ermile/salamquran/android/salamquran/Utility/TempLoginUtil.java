02cb39ca5ceacd87472d0ffea383d752e1097a0a
amingoli78 <amin.goli78@gmail.com>
2019-12-18 18:58:02 +0330

    add try catch

--- quran_android-2.9.5-p1/app/src/main/java/com/ermile/salamquran/android/salamquran/Utility/TempLoginUtil.java
+++ quran_android-2.9.5-p1/app/src/main/java/com/ermile/salamquran/android/salamquran/Utility/TempLoginUtil.java
@@ -8,23 +8,28 @@ public class TempLoginUtil {
   private Context context;
   public TempLoginUtil(Context context) {
     this.context = context;
-    if (!UserInfo.userIdAdded(context)){
-      Api.getToken(new Api.getTokenListener() {
-        @Override
-        public void onReceived(String token) {
-          Api.userAdd(context, token,
-              new Api.userAddListener() {
-                @Override
-                public void onReceived() {}
-                @Override
-                public void onMassage(String massage) {}
-                @Override
-                public void onFailed() {}
-              });
-        }
-        @Override
-        public void onFailed(String error) {}
-      });
+    try {
+      if (!UserInfo.userIdAdded(context)){
+        Api.getToken(new Api.getTokenListener() {
+          @Override
+          public void onReceived(String token) {
+            Api.userAdd(context, token,
+                new Api.userAddListener() {
+                  @Override
+                  public void onReceived() {}
+                  @Override
+                  public void onMassage(String massage) {}
+                  @Override
+                  public void onFailed() {}
+                });
+          }
+          @Override
+          public void onFailed(String error) {}
+        });
+      }
+    }catch (Exception e){
+      e.printStackTrace();
     }
+
   }
 }
