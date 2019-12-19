a989869659f9016b7848efb364783b6a4d84c3d5
amingoli78 <amin.goli78@gmail.com>
2019-12-20 00:51:14 +0330

    optimizing

--- quran_android-2.9.5-p1/app/src/main/java/com/ermile/salamquran/android/salamquran/Utility/Url.java
+++ quran_android-2.9.5-p1/app/src/main/java/com/ermile/salamquran/android/salamquran/Utility/Url.java
@@ -7,54 +7,47 @@ import com.ermile.salamquran.android.salamquran.Utility.SaveManager;
 public class Url {
   public static String site = "https://salamquran.com";
   public static String api = site+"/api/v6";
+  public static String apiV6 = "/api/v6";
 
   public static String getLocal(Context context){
-    return SaveManager.get(context).getstring_appINFO().get(SaveManager.local);
+    return site + "/"
+            + SaveManager.get(context).getstring_appINFO().get(SaveManager.appLanguage)
+            + apiV6;
   }
 //Learn
   public static String getLmsGroup(Context context){
-    String lms_group = "/lms/group";
-    return getLocal(context) + lms_group;
+    return getLocal(context) + "/lms/group";
   }
   public static String getLmsLevelList(Context context, String id){
-    String lms_level_list = "/lms/levellist?id="+id;
-    return getLocal(context) + lms_level_list;
+    return getLocal(context) + "/lms/levellist?id="+id;
   }
   public static String getLmsLevelInfo(Context context, String id){
-    String lms_level = "/lms/level?id="+id;
-    return getLocal(context) + lms_level;
+    return getLocal(context) + "/lms/level?id="+id;
   }
 //Mag
   public static String getMagList(Context context, int limit){
-    String lms_level = "/posts?limit="+limit;
-    return getLocal(context) + lms_level;
+    return getLocal(context) + "/posts?limit="+limit ;
   }
 //Token & Login
   public static String getToken(){
-    String token = "/token";
-    return api + token;
+    return api + "/token";
   }
   public static String getUserAdd(Context context){
-    String user_add = "/android/user/add";
-    return getLocal(context) + user_add;
+    return getLocal(context) + "/android/user/add";
   }
 // Aya & Page Day
   public static String getAyaDay(Context context){
-    String aya_day = "/aya/day";
-    return getLocal(context) + aya_day;
+    return getLocal(context) + "/aya/day";
   }
   public static String getPageDay(Context context){
-    String page_day = "/page/day";
-    return getLocal(context) + page_day;
+    return getLocal(context) + "/page/day";
   }
 //  Notification
   public static String getSmile(Context context){
-    String smile = "/smile";
-    return getLocal(context) + smile;
+    return getLocal(context) + "/smile";
   }
   public static String getNotif(Context context){
-    String notif = "/notif";
-    return getLocal(context) + notif;
+    return getLocal(context) + "/notif";
   }
 
 
