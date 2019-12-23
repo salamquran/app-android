abcd24bf48ea03a95a28bed88f75e6419e962cb9
amingoli78 <amin.goli78@gmail.com>
2019-12-23 17:03:52 +0330

    fix Bug SearchView

--- quran_android-2.9.5-p1/app/src/main/java/com/ermile/salamquran/android/salamquran/SearchFragment.java
+++ quran_android-2.9.5-p1/app/src/main/java/com/ermile/salamquran/android/salamquran/SearchFragment.java
@@ -4,24 +4,15 @@ package com.ermile.salamquran.android.salamquran;
 import android.app.SearchManager;
 import android.content.ComponentName;
 import android.content.Context;
-import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Bundle;
-
-import android.text.Html;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.SearchView;
-
-import androidx.core.content.ContextCompat;
-import androidx.core.view.MenuItemCompat;
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
-import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import androidx.appcompat.widget.SearchView;
+import androidx.fragment.app.Fragment;
 
 import com.ermile.salamquran.android.QuranApplication;
 import com.ermile.salamquran.android.R;
@@ -34,6 +25,8 @@ import java.util.Objects;
  */
 public class SearchFragment extends Fragment {
 
+  SearchView searchView;
+  SearchManager searchManager;
 
   public SearchFragment() {
     // Required empty public constructor
@@ -55,25 +48,34 @@ public class SearchFragment extends Fragment {
 
 
 
-    final SearchView searchView = view.findViewById(R.id.searchView);
+    searchView = view.findViewById(R.id.searchView);
 
-    int id = searchView
+/*    int id = searchView
         .getContext()
         .getResources()
         .getIdentifier("android:id/search_src_text", null, null);
     TextView textView = (TextView) searchView.findViewById(id);
 //    textView.setTextColor(Color.WHITE);
     Typeface typeface = Typeface.createFromAsset(getContext().getAssets(),"dana_regular.ttf");
-    textView.setTypeface(typeface);
+    textView.setTypeface(typeface);*/
 
-    final SearchManager searchManager =
-        (SearchManager) getActivity().getSystemService(Context.SEARCH_SERVICE);
-    searchView.setQueryHint(getString(R.string.search_hint));
+    searchManager = (SearchManager)
+        Objects.requireNonNull(getActivity()).getSystemService(Context.SEARCH_SERVICE);
     searchView.setSearchableInfo(searchManager.getSearchableInfo(
         new ComponentName(getActivity().getApplication(), SearchActivity.class)));
+
+    searchView.setQueryHint(getString(R.string.search_hint));
+    s();
+    view.findViewById(R.id.box_search).setOnClickListener(v -> s());
 
 
     return view;
   }
 
+  private void s(){
+    searchView.setQuery(null,false);
+    searchView.setFocusable(true);
+    searchView.setIconified(false);
+    searchView.requestFocusFromTouch();
+  }
 }
