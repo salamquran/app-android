78733bb69da37c71ab45cbe7fdd993070a76af5c
amingoli78 <amin.goli78@gmail.com>
2019-12-23 17:17:37 +0330

    Remove Bottom Line

--- quran_android-2.9.5-p1/app/src/main/java/com/ermile/salamquran/android/salamquran/SearchFragment.java
+++ quran_android-2.9.5-p1/app/src/main/java/com/ermile/salamquran/android/salamquran/SearchFragment.java
@@ -4,12 +4,10 @@ package com.ermile.salamquran.android.salamquran;
 import android.app.SearchManager;
 import android.content.ComponentName;
 import android.content.Context;
-import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
@@ -25,8 +23,8 @@ import java.util.Objects;
  */
 public class SearchFragment extends Fragment {
 
-  SearchView searchView;
-  SearchManager searchManager;
+  private SearchView searchView;
+  private SearchManager searchManager;
 
   public SearchFragment() {
     // Required empty public constructor
@@ -45,34 +43,18 @@ public class SearchFragment extends Fragment {
                            Bundle savedInstanceState) {
     // Inflate the layout for this fragment
     View view = inflater.inflate(R.layout.fragment_search, container, false);
-
-
-
     searchView = view.findViewById(R.id.searchView);
-
-/*    int id = searchView
-        .getContext()
-        .getResources()
-        .getIdentifier("android:id/search_src_text", null, null);
-    TextView textView = (TextView) searchView.findViewById(id);
-//    textView.setTextColor(Color.WHITE);
-    Typeface typeface = Typeface.createFromAsset(getContext().getAssets(),"dana_regular.ttf");
-    textView.setTypeface(typeface);*/
-
+    searchView.setQueryHint(getString(R.string.search_hint));
     searchManager = (SearchManager)
         Objects.requireNonNull(getActivity()).getSystemService(Context.SEARCH_SERVICE);
     searchView.setSearchableInfo(searchManager.getSearchableInfo(
         new ComponentName(getActivity().getApplication(), SearchActivity.class)));
-
-    searchView.setQueryHint(getString(R.string.search_hint));
-    s();
-    view.findViewById(R.id.box_search).setOnClickListener(v -> s());
-
-
+    setFocusSearchView();
+    view.findViewById(R.id.box_search).setOnClickListener(v -> setFocusSearchView());
     return view;
   }
 
-  private void s(){
+  private void setFocusSearchView(){
     searchView.setQuery(null,false);
     searchView.setFocusable(true);
     searchView.setIconified(false);
